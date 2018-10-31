4ee5b1a08cb0eab80a528251e0e4c8ea6f383c11
madhavibarve <44630274+madhavibarve@users.noreply.github.com>
2018-10-30 23:51:53 -0500

    Add files via upload

---
+++ BlindsTestCases/TestCase1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace TestCase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.blinds.com");
+            driver.Manage().Window.Maximize();
+
+            IWebElement elementsearchbox = driver.FindElement(By.Id("gcc-typeahead-input"));
+            IWebElement elementsearchbutton = driver.FindElement(By.Id("gcc-typeahead-submit"));
+
+            //Populate search key
+            elementsearchbox.SendKeys("Room Darkening Blinds");
+
+            //Click search button
+            driver.FindElement(By.Id("gcc-typeahead-submit")).Click();       
+
+            //Search results are displayed now
+            // Find and click appropriate hyperlink to sort by Price Low-To High
+            driver.FindElement(By.LinkText("Price Low-High")).Click();
+        }
+    }
+}
